32119ac524713f1243094a77bb23bcebc376f9fd
Soumajit Choudhury <soumajitchoudhury18@gmail.com>
2025-07-15 14:57:35 +0530

    updated errors 6

--- run_windows.bat
+++ run_windows.bat
@@ -1,20 +1,10 @@
-@echo off
-echo Setting up NER Project environment...
 
-echo Creating virtual environment...
 python -m venv venv
 
-echo Activating virtual environment...
-call venv\Scripts\activate
+venv\Scripts\activate
 
-echo Installing required packages...
 pip install -r requirements.txt
 
-echo Downloading spaCy model...
 python -m spacy download en_core_web_sm
 
-echo Starting Streamlit application...
 streamlit run app.py
-
-echo.
-echo When finished, type 'deactivate' to exit the virtual environment
